054b7c3478126faa6e0ab35554310fb41c2cf692
bryan967132 <bryan.tejaxun@gmail.com>
2024-02-01 00:11:19 -0600

    Comandos

---
+++ comandos.sh
@@ -0,0 +1,14 @@
+# CREAR IMAGEN DE BACK
+docker build -t back_t1 .
+# CREAR IMAGEN DE FRONT
+docker build -t front_t1 .
+# VERIFICAR IMÁGENES EXISTENTES
+docker images
+# EJECUTAR IMAGEN BACK
+docker run -d -p 8000:8000 back_t1
+# EJECUTAR IMAGEN FRONT
+docker run -d -p 3000:3000 front_t1
+# VERIFICAR CONTENEDORES EJECUTÁNDOSE
+docker ps
+# DETENER EJECUCIÓN DE CONTENEDOR
+docker stop <ID_CONTENEDOR>
