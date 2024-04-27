fa4ee82296143b3c2aec24d982477988d0c96c48
bryan967132 <bryan.tejaxun@gmail.com>
2024-04-27 15:45:08 -0600

    Consumer

--- Proyecto2/Consumer/main.go
+++ Proyecto2/Consumer/main.go
@@ -2,32 +2,110 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
 	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/go-redis/redis/v8"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Data struct {
-	Album  string `json:"Album"`
-	Year   string `json:"Year"`
-	Artist string `json:"Artist"`
-	Ranked string `json:"Ranked"`
+	Name  string `json:"Name"`
+	Album string `json:"Album"`
+	Year  string `json:"Year"`
+	Rank  string `json:"Rank"`
 }
 
-type Log struct {
-	Data string `bson:"data"`
+func getRedisClient() (context.Context, *redis.Client) {
+	var ctx context.Context = context.Background()
+	var client *redis.Client = redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: "YOUR_PASSWORD",
+		DB:       0,
+	})
+
+	pong, err := client.Ping(context.Background()).Result()
+	if err != nil {
+		fmt.Println("Error al hacer ping a Redis:", err)
+		return nil, nil
+	}
+	fmt.Println("Respuesta de Redis:", pong)
+	return ctx, client
+}
+
+func getMongoConnection() (context.Context, *mongo.Collection) {
+	var ctx context.Context = context.Background()
+	var client, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://mongodb:27017"))
+	if err != nil {
+		fmt.Printf("Failed to connect to MongoDB: %s", err)
+		return nil, nil
+	}
+	defer client.Disconnect(ctx)
+
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		fmt.Println("Error al hacer ping a MongoDB:", err)
+		return nil, nil
+	}
+	fmt.Println("Conexión exitosa a MongoDB. Ping OK.")
+
+	var logCollection *mongo.Collection = client.Database("bdso1p2").Collection("logs")
+	return ctx, logCollection
+}
+
+func convertData(data string) *Data {
+	var newData Data
+	err := json.Unmarshal([]byte(data), &newData)
+	if err != nil {
+		fmt.Println("Error al convertir la cadena a JSON:", err)
+		return nil
+	}
+	return &newData
+}
+
+func insertRedis(ctx context.Context, rdb *redis.Client, data *Data) error {
+	hashKey := fmt.Sprintf("%s:%s:%s", data.Name, data.Album, data.Year)
+	fmt.Printf("hashkey new vote = %v\n", hashKey)
+
+	err := rdb.HIncrBy(ctx, "albums", hashKey, 1).Err()
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Redis -> Se agregó album: %v, hashkey: %v\n", data.Album, hashKey)
+
+	_, err = rdb.Incr(ctx, "total").Result()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Redis -> Se incrementó el total de votos")
+
+	return nil
+}
+
+func insertMongo(ctx context.Context, collection *mongo.Collection, data *Data) error {
+	log := bson.M{
+		"Name": data.Name, "ALbum": data.Album, "Year": data.Year, "Rank": data.Rank,
+	}
+
+	_, err := collection.InsertOne(ctx, log)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Mongo -> Log insertado en MongoDB: %v\n", data)
+	return nil
 }
 
 func main() {
-	// Configuración de Kafka
 	c, err := kafka.NewConsumer(&kafka.ConfigMap{
 		"bootstrap.servers": "my-cluster-kafka-bootstrap:9092",
 		"group.id":          "mygroupid",
@@ -38,48 +116,9 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Configuración de Redis
-	ctx := context.Background()
+	var redisCtx, redisClient = getRedisClient()
 
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     "redis:6379",
-		Password: "YOUR_PASSWORD",
-		DB:       0,
-	})
-
-	// Realiza un ping a Redis
-	pong, err := rdb.Ping(context.Background()).Result()
-	if err != nil {
-		fmt.Println("Error al hacer ping a Redis:", err)
-		return
-	}
-	fmt.Println("Respuesta de Redis:", pong)
-
-	// Configuración de MongoDB
-	mongoURI := "mongodb://mongodb:27017"
-	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
-	if err != nil {
-		fmt.Printf("Failed to create MongoDB client: %s", err)
-		os.Exit(1)
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	err = client.Connect(ctx)
-	if err != nil {
-		fmt.Printf("Failed to connect to MongoDB: %s", err)
-		os.Exit(1)
-	}
-	defer client.Disconnect(ctx)
-
-	err = client.Ping(context.Background(), nil)
-	if err != nil {
-		fmt.Println("Error al hacer ping a MongoDB:", err)
-		return
-	}
-	fmt.Println("Conexión exitosa a MongoDB. Ping OK.")
-
-	database := client.Database("bdso1p2")
-	logCollection := database.Collection("logs")
+	var mongoCtx, mongoCollection = getMongoConnection()
 
 	topic := "topic-so1p2"
 	err = c.SubscribeTopics([]string{topic}, nil)
@@ -91,7 +130,6 @@ func main() {
 	sigchan := make(chan os.Signal, 1)
 	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
 
-	// Función para consumir mensajes de Kafka,  actualizar Redis y insertar mongo
 	go func() {
 		for {
 			select {
@@ -105,17 +143,15 @@ func main() {
 				if err != nil {
 					continue
 				}
-				fmt.Printf("Consumed event from topic %s: key = %-10s value = %s\n",
-					*ev.TopicPartition.Topic, string(ev.Key), string(ev.Value))
 
-				// Actualizar datos en Redis
-				err = processAndUpdateRedis(ctx, rdb, string(ev.Value))
+				fmt.Printf("Consumed event from topic %s: value = %s\n", *ev.TopicPartition.Topic, string(ev.Value))
+
+				err = insertRedis(redisCtx, redisClient, convertData(string(ev.Value)))
 				if err != nil {
 					fmt.Printf("Failed to process and update Redis: %s\n", err)
 				}
 
-				// Insertar log en MongoDB
-				err = insertLog(ctx, logCollection, string(ev.Value))
+				err = insertMongo(mongoCtx, mongoCollection, convertData(string(ev.Value)))
 				if err != nil {
 					fmt.Printf("Failed to insert log into MongoDB: %s\n", err)
 				}
@@ -124,48 +160,5 @@ func main() {
 		}
 	}()
 
-	// Mantener el programa en ejecución
 	<-sigchan
-}
-
-func processAndUpdateRedis(ctx context.Context, rdb *redis.Client, data string) error {
-	// Procesar la cadena de datos para extraer los valores
-	values := strings.Split(data, ", ")
-	name := strings.Split(values[0], ": ")[1]
-	album := strings.Split(values[1], ": ")[1]
-	year := strings.Split(values[2], ": ")[1]
-
-	// Clave del hash en Redis
-	hashKey := fmt.Sprintf("albums:%s:%s:%s", name, album, year)
-	fmt.Printf("hashkey new vote = %v\n", hashKey)
-
-	// Actualizar el contador de votos en Redis
-	err := rdb.HIncrBy(ctx, "albums", hashKey, 1).Err()
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("Se agregó albums:" + album + " hashkey: " + hashKey)
-	return nil
-}
-
-func insertLog(ctx context.Context, collection *mongo.Collection, data string) error {
-	// Procesar la cadena de datos para extraer los valores
-	values := strings.Split(data, ", ")
-	name := strings.Split(values[0], ": ")[1]
-	album := strings.Split(values[1], ": ")[1]
-	year := strings.Split(values[2], ": ")[1]
-	rank := strings.Split(values[3], ": ")[1]
-	data = "ALbum: " + album + " Name: " + name + " year: " + year + " rank: " + rank
-	log := Log{
-		Data: data,
-	}
-
-	_, err := collection.InsertOne(ctx, log)
-	if err != nil {
-		return err
-	}
-
-	fmt.Println("Log insertado en MongoDB: " + data)
-	return nil
 }
